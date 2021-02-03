a3c55e5c7275a4c1e2b6eb7bf8d82454de064bbb
Gaurav Patil <57212399+Gaurav3435@users.noreply.github.com>
2021-02-03 23:01:17 +0530

    Update urls.py

--- webapp/urls.py
+++ webapp/urls.py
@@ -3,7 +3,7 @@ from django.urls import path,include
 from . import views
 from django.views.static import serve
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from django.conf.urls import url
+
 urlpatterns = [
     path('',views.start_ ),
     path('contact/',views.contact_form),
@@ -11,8 +11,7 @@ urlpatterns = [
     path('research/<int:myid>',views.blog_detail),
     path('project/<int:myid>',views.blog_detail),
     path('r_project/<int:myid>',views.blog_detail),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+
 
 ]
 urlpatterns+=staticfiles_urlpatterns()
